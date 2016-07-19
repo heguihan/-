300eb1c1b936fae8e91d40babb5f66d78bc747a1
金视和 <jinshihe@jinshihedeimac.local>
2016-07-19 11:05:02 +0800

    Initial Commit

---
+++ 状态栏/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  状态栏
+//
+//  Created by 金视和 on 16/7/19.
+//  Copyright © 2016年 金视和. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 状态栏/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  状态栏
+//
+//  Created by 金视和 on 16/7/19.
+//  Copyright © 2016年 金视和. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
